0c20a45eaa57af06db1f59db7bacdf673f1be140
Peter Kurfer <peter.kurfer@fh-rosenheim.de>
2018-12-07 11:14:02 +0100

    Added default JavaFX project structure

---
+++ src/main/java/de/thro/inf/prg3/a10/App.java
@@ -0,0 +1,23 @@
+package de.thro.inf.prg3.a10;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+    	launch(args);
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// note: var is the dynamic type; specify template argument b/c compiler can't infer it!
+    	var root = FXMLLoader.<Parent>load(getClass().getResource("views/main.fxml"));
+    	primaryStage.setTitle("THRO Diner");
+    	primaryStage.setScene(new Scene(root, 800, 600));
+    	primaryStage.show();
+	}
+}
---
+++ src/main/java/de/thro/inf/prg3/a10/controller/MainController.java
@@ -0,0 +1,14 @@
+package de.thro.inf.prg3.a10.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
